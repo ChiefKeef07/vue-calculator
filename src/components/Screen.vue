fd26ee0175b0f491740f17f79ca18595e3dc1bce
Unknown <keefer.alex77@gmail.com>
2019-08-05 23:16:23 -0400

    initial start of the calculator

---
+++ src/components/Screen.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div>
+            <input type="number" v-model="number"/>
+        </div>
+        <div>
+            <span>{{result}}</span>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Screen',
+    props: {
+
+    },
+    data() {
+        return {
+            number: 0,
+            result: 0,
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
